f180e39865f81a717b1064a2569bd91ddd9ccc84
Alfonso Rossell Rurush Rosas <owlrossell@hotmail.com>
2023-10-29 22:14:56 -0500

    Condición de no existencia filtrada y ocultada

--- src/api/product.ts
+++ src/api/product.ts
@@ -5,5 +5,5 @@ const API_URL = process.env.API_URL;
 export const getAllProducts = async (): Promise<ProductLocal[]> => {
     const response = await fetch(`${API_URL}/products`);
     const {data} = await response.json();
-    return data.map((product: ProductApi) => toProductLocal(product));
+    return data.map((product: ProductApi) => toProductLocal(product)).filter((product: ProductLocal) => product.stock > 0);
 }
